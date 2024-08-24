862d3c5bd1e3347b181d1a2298f724e062448523
soumyadeep1143 <mondalsoumyadeep4@gmail.com>
2024-08-25 02:14:35 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,16 +30,18 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-
+//template_h5t3ao5
+//service_jp0kry4
+//aOY_pvgZZV1_7LgpS
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Soumyadeep Mondal",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "mondalsoumyadeep4@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
